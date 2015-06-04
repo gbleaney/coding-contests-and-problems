f062bb91299195da2b67e9b439824935fe3c2ffd
Graham Bleaney <gbleaney@gmail.com>
2015-06-04 09:53:26 -0400

    Added bootstrapping project

---
+++ Bootstrap Project/Bootstrap Project/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bootstrap_Project
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int numCases = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < numCases; i++)
+            {
+                string input = Console.ReadLine();
+            }
+
+        }
+    }
+}
