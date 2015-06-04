4b8288a01ea6bcf43956d3a45e3326034dac9fdf
Graham Bleaney <gbleaney@gmail.com>
2015-06-04 09:55:26 -0400

    Set up for square interview

---
+++ Square Interview 3A/Square Interview 3A/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Square_Interview_3A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunTests();
+        }
+
+        static void DoSomething()
+        {
+            
+        }
+
+        static void RunTests()
+        {
+            DoSomething();
+        }
+    }
+}
