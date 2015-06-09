707b0c70b240b728b297f04682726ecb356d721e
Graham Bleaney <gbleaney@gmail.com>
2015-06-08 23:32:59 -0400

    Finished linkedin iterator question

--- LinkedIn List with Sublist Iterator 3A/LinkedIn List with Sublist Iterator 3A/Program.cs
+++ LinkedIn List with Sublist Iterator 3A/LinkedIn List with Sublist Iterator 3A/Program.cs
@@ -9,26 +9,35 @@ namespace LinkedIn_List_with_Sublist_Iterator_3A
 {
     class Data<T>
     {
-        private IEnumerable<Data<T>> collection;
-        private T value;
-        private bool isCollection;
+        public Data(IEnumerable<Data<T>> collection)
+        {
+            this.collection = collection;
+            this.isCollection = true;
+        }
+
+        public Data(T value)
+        {
+            this.value = value;
+            this.isCollection = false;
+        } 
+
+        private readonly IEnumerable<Data<T>> collection;
+        private readonly T value;
+        private readonly bool isCollection;
 
         public IEnumerable<Data<T>> Collection
         {
             get { return collection; }
-            set { collection = value; }
         }
 
         public bool IsCollection
         {
             get { return isCollection; }
-            set { isCollection = value; }
         }
 
         public T Value
         {
             get { return value; }
-            set { this.value = value; }
         }
     }
 
@@ -47,23 +56,35 @@ namespace LinkedIn_List_with_Sublist_Iterator_3A
             currentEnumerator = collection.GetEnumerator();
             currentEnumerator.MoveNext(); // Get to the first valid element
 
+            currentEnumeratorHasNext = true;
+
             MoveNext();
         }
 
         private void MoveNext()
         {
-            currentData = currentEnumerator.Current;
-
-
-            if (currentData.IsCollection)
+            if (currentEnumeratorHasNext)
             {
-                curreDataIterator = new DataIterator<T>(currentData.Collection);
+                currentData = currentEnumerator.Current;
+
+
+                if (currentData.IsCollection)
+                {
+                    curreDataIterator = new DataIterator<T>(currentData.Collection);
+                }
+                currentEnumeratorHasNext = currentEnumerator.MoveNext();
+            }
+            else
+            {
+                currentData = null;
+                curreDataIterator = null;
+                currentEnumeratorHasNext = false;
             }
 
-            currentEnumeratorHasNext = currentEnumerator.MoveNext();
+            
         }
 
-        bool HasNext()
+        public bool HasNext()
         {
             if (currentData == null)
             {
@@ -90,28 +111,23 @@ namespace LinkedIn_List_with_Sublist_Iterator_3A
             }
         }
 
-        T Next()
+        public T Next()
         {
-            if (currentData.IsCollection)
+            T retVal;
+
+            if (!currentData.IsCollection)
             {
-                if (curreDataIterator.HasNext())
-                {
-                    return curreDataIterator.Next();
-                }
-                else if (currentEnumeratorHasNext)
-                {
-                    
-                }
-                else
-                {
-                    throw new Exception("End of iterator");
-                }
-                
-            }
-            else
+                retVal =  currentData.Value;
+                MoveNext();
+            } else
             {
-                throw new Exception("End of iterator");
+                retVal = curreDataIterator.Next();
+                if (!curreDataIterator.HasNext())
+                {
+                    MoveNext();
+                }
             }
+            return retVal;
         }
 
     }
@@ -120,6 +136,25 @@ namespace LinkedIn_List_with_Sublist_Iterator_3A
     {
         static void Main(string[] args)
         {
+            Data<int>[] data1 = {new Data<int>(1), new Data<int>(2), new Data<int>(3)};
+            Data<int> data2 = new Data<int>(4);
+            Data<int>[] data3 = { new Data<int>(5), new Data<int>(6), new Data<int>(7) };
+            Data<int>[] data4 = { new Data<int>(8), new Data<int>(9), new Data<int>(10) };
+            Data<int>[] data5 = {new Data<int>(data3), new Data<int>(data4),};
+
+            List<Data<int>> data = new List<Data<int>>();
+            data.Add(new Data<int>(data1));
+            data.Add(data2);
+            data.Add(new Data<int>(data5));
+
+            DataIterator<int> iterator = new DataIterator<int>(data);
+
+            while (iterator.HasNext())
+            {
+                Console.WriteLine(iterator.Next());
+            }
+
+            Console.ReadLine();
         }
     }
 }
