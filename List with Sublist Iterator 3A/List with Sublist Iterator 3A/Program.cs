88600310bb7b6fe37a19b1fd49d39ab239ae7693
Graham Bleaney <gbleaney@gmail.com>
2015-06-09 22:10:53 -0400

    Removing company references

--- List with Sublist Iterator 3A/List with Sublist Iterator 3A/Program.cs
+++ List with Sublist Iterator 3A/List with Sublist Iterator 3A/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LinkedIn_List_with_Sublist_Iterator_3A
+namespace List_with_Sublist_Iterator_3A
 {
     class Data<T>
     {
